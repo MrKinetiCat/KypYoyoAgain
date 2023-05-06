8446cf089281f4d9b2f0dfd2e8d83444d1e827fe
Toby Kevin <Giddomh@gmail.com>
2023-05-06 16:20:03 -0400

    e
    dsfg

--- Content/Tiles/Furniture/YoyoSmithingStation.cs
+++ Content/Tiles/Furniture/YoyoSmithingStation.cs
@@ -39,8 +39,8 @@ namespace KypYoyo.Content.Tiles.Furniture
 
 
             TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 18 };
-            TileObjectData.newTile.Origin = new Point16(2, 2);
-
+            TileObjectData.newTile.Origin = new Point16(0, 0);
+            //shdjg
             TileObjectData.addTile(Type);
 
             LocalizedText YoyoSmithingStation = CreateMapEntryName();
